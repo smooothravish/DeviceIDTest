15b863211c5fd215f8f35c48817f38b71294f345
Pranav Prakash <pranav.prakash@klarna.com>
2024-03-26 09:43:39 +0100

    Incognia, TMX and FPJS integrations fixing JS file

--- klarna_toolkit.js
+++ klarna_toolkit.js
@@ -1,104 +1,134 @@
-<!doctype html>
-<html class="no-js" lang="">
-
-  <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title></title>
-<link rel="stylesheet" href="css/style.css">
-  <meta name="description" content="">
-
-    <meta property="og:title" content="">
-      <meta property="og:type" content="">
-        <meta property="og:url" content="">
-          <meta property="og:image" content="">
-
-            <link rel="icon" href="/favicon.ico" sizes="any">
-              <link rel="icon" href="/icon.svg" type="image/svg+xml">
-                <link rel="apple-touch-icon" href="icon.png">
-
-                  <link rel="manifest" href="site.webmanifest">
-                    <meta name="theme-color" content="#fafafa">
-                      <script>
-                        window.addEventListener('DOMContentLoaded', () =>
-                        window.IncogniaWebSdk.init('3048e4b7-5938-49d8-abeb-4bee270bdcca')
-                        )
-                      </script>
-
-                    </head>
-
-                    <body>
-
-                    <!-- Add your site or application content here -->
-                    <p>Hello world! This is HTML5 Boilerplate.</p>
-                    <script src="js/app.js"></script>
-                    <script src="js/klarna_toolkit.js"></script>
-                    <script defer src="https://repo.incognia.com/web/latest/incognia-web-sdk.js"></script>
-                    <script>
-                      // Initialize the agent on page load.
-                      const fpPromise = import('https://fpjscdn.net/v3/cLUP4yT1dAUJAO90UKgp')
-                      .then(FingerprintJS => FingerprintJS.load({
-                      region: "eu"
-                    }))
-
-                      // Get the visitorId when you need it.
-                      fpPromise
-                      .then(fp => fp.get())
-                      .then(result => {
-                      const visitorId = result.visitorId
-                      console.log(visitorId)
-                    })
-
-                    </script>
-                    <script>
-                      window.onload = function() {
-                      try {
-                      if (typeof klarna.profile === 'function') {
-                      //var y = 'sessiontest34'
-                      var x = klarna.profile('h.online-metrix.net', '87rxrdob', 'sessiontest63');
-                      console.log(x);
-                      console.log('sessionid -  ses123');
-                    } else {
-                      console.log('klarna.profile is not defined');
-                    }
-                    } catch(err){
-                      console.log('error: ' + err);
-                    }
-                    };
-                    </script>
-                    <input type="text" id="myTextBox">
-                      <button id="myButton" onclick="callAPI()">Submit</button>
-
-                      <p id="displayText">Your text will appear here</p>
-
-
-
-                      <script>
-                        function callAPI() {
-                        // Assuming you're sending the text as a query parameter to a GET endpoint
-                        var userInput = document.getElementById('myTextBox').value;
-                        var apiURL = 'https://h-api-sb.online-metrix.net/api/session-query?org_id=87rxrdob&api_key=i4lqdshhsbmz0kww&session_id=sessiontest63&service_type=session-policy&account_telephone=' + encodeURIComponent(userInput);
-
-                        fetch(apiURL)
-                        .then(response => {
-                        if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                      }
-                        return response.json(); // or .text() if the response is plain text
-                      })
-                        .then(data => {
-                        console.log(data); // Process your data here
-                        document.getElementById('displayText').innerText = JSON.stringify(data); // Example of displaying the response
-                      })
-                        .catch(error => {
-                        console.error('There has been a problem with your fetch operation:', error);
-                      });
-                        document.getElementById('displayText').innerText = userInput + apiURL;
-                      }
-
-
-                      </script>
-
-                    </body>
-
-                  </html>
+(function() {
+  var g = this || self;
+  function z() {
+    return "undefined" === typeof Date.now ? (new Date).getTime() : Date.now()
+  }
+  function N(E) {
+    this.L = E;
+    16 == this.L ? (this.v = 268435456, this.C = 4026531839) : (this.v = 78364164096, this.C = 2742745743359)
+  }
+  function l(E) {
+    return (Math.floor(Math.random() * E.C) + E.v).toString(E.L)
+  };
+  function T(E) {
+    this.C = E
+  }
+  T.prototype.supported = function() {
+    return void 0 != window.localStorage
+  };
+  T.prototype.get = function() {
+    return window.localStorage.getItem(this.C)
+  };
+  T.prototype.set = function(E) {
+    return window.localStorage.setItem(this.C, E)
+  };
+  T.prototype.set = T.prototype.set;
+  function Z() {
+    var E = z(),
+      Y = new N(16);
+    Y = l(Y) + l(Y) + l(Y) + l(Y);
+    return [0, 0, E, E, Y].join(":")
+  }
+  function J() {
+    var E = new T("ed73f20edbf2b73");
+    if (!E.supported()) return null;
+    E = E.get();
+    if (null === E) return null;
+    var Y = E.split("_");
+    2 === Y.length && (E = Y[0]);
+    return "0:" + E
+  }
+  function v() {
+    var E = J();
+    if (null === E)
+      if (E = new T("ed73f20edbf2b74"), E.supported()) {
+        var Y = E.get();
+        null === Y && (Y = Z());
+        var u = E.set;
+        var S = Y.split(":");
+        if (5 != S.length) S = Y;
+        else {
+          var t = parseInt(S[1], 10) + 1,
+            K = z();
+          K > parseInt(S[2], 10) + 157788E5 ? S = Z() : (S[1] = t.toString(), S[3] = K, S = S.join(":"))
+        }
+        u.call(E, S);
+        E = "1:" + Y
+      } else E = null;
+    return E
+  }
+  function y(E, Y) {
+    E = {
+      iceServers: [{
+        urls: "turn:aa.online-metrix.net?transport\x3dtcp",
+        username: E,
+        credential: Y
+      }, {
+        urls: "turn:aa.online-metrix.net?transport\x3dudp",
+        username: E,
+        credential: Y
+      }]
+    };
+    return "undefined" !== typeof window.RTCPeerConnection && null !== window.RTCPeerConnection ? new window.RTCPeerConnection(E) : "undefined" !== typeof window.webkitRTCPeerConnection && null !== window.webkitRTCPeerConnection ? new window.webkitRTCPeerConnection(E) : "undefined" !== typeof window.C && null !== window.C ? new window.C(E) : null
+  }
+  function w(E, Y) {
+    var u = v();
+    try {
+      var S = y("2:" + E + ":" + Y + ":" + u, Y);
+      if (S && "undefined" !== typeof S.createDataChannel && null !== S.createDataChannel) {
+        S.createDataChannel(Math.random().toString());
+        var t = function() {};
+        E = function(K) {
+          S.setLocalDescription(K, t, t)
+        };
+        "undefined" === typeof Promise || 0 < S.createOffer.length ? S.createOffer(E, t) : S.createOffer().then(E, t);
+        setInterval(function() {
+          "undefined" !== typeof S.close && null !== S.close && S.close();
+          "undefined" !== typeof S.onicecandidate && null !== S.onicecandidate && (S.onicecandidate =
+            function() {}, S = null)
+        }, 1E4)
+      }
+    } catch (K) {}
+  }
+  var X = null;
+  function h(E) {
+    for (var Y; null !== (Y = document.getElementById(E));) Y.parentElement.removeChild(Y)
+  }
+  function P(E, Y, u, S) {
+    if ("undefined" !== typeof E && "undefined" !== typeof Y && "undefined" !== typeof u && 8 === Y.length) {
+      if (-1 !== u.indexOf(":")) throw Error("invalid session_id " + u);
+      h("tdz_ifrm");
+      h("tmx_tags_iframe");
+      h("tmx_tags_js");
+      var t = document;
+      if ("undefined" !== typeof t.currentScript && null !== t.currentScript) {
+        var K = t.currentScript.getAttribute("nonce");
+        "undefined" !== typeof K && null !== K && "" !== K ? X = K : "undefined" !== typeof t.currentScript.nonce && null !== t.currentScript.nonce && "" !== t.currentScript.nonce &&
+          (X = t.currentScript.nonce)
+      }
+      w(Y, u);
+      t = document.getElementsByTagName("head").item(0);
+      K = document.createElement("script");
+      K.id = "tmx_tags_js";
+      K.setAttribute("type", "text/javascript");
+      var k = new N(36),
+        I = Math.floor(Math.random() * k.C),
+        n = Math.floor(Math.random() * k.C);
+      n = (n - n % 256 + 5 + I) % k.C;
+      var L = (885187064159 + I) % k.C;
+      I = l(k) + (I + k.v).toString(k.L);
+      n = (L + k.v).toString(k.L) + (n + k.v).toString(k.L);
+      E = "https://" + E + "/" + I + ".js";
+      Y = [n + "\x3d" + Y, l(k) + l(k) + "\x3d" + u];
+      "undefined" !== typeof S && null !== S && 0 < S.length && Y.push(l(k) + l(k) +
+        "\x3d" + S);
+      K.setAttribute("src", E + "?" + Y.join("\x26"));
+      null !== X && (K.setAttribute("nonce", X), K.getAttribute("nonce") !== X && (K.nonce = X));
+      t.appendChild(K)
+    }
+  }
+  var W = ["klarna", "profile"],
+    Q = g;
+  W[0] in Q || "undefined" == typeof Q.execScript || Q.execScript("var " + W[0]);
+  for (var R; W.length && (R = W.shift());) W.length || void 0 === P ? Q[R] && Q[R] !== Object.prototype[R] ? Q = Q[R] : Q = Q[R] = {} : Q[R] = P;
+}).call(this);
